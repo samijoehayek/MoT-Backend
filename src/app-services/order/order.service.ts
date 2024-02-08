9a052db6ee614b71957f7e24b6933d2dd10f5939
Mindflares <mindflares@Mindflaress-MacBook-Pro.local>
2024-02-08 17:02:45 +0200

    Added order CRUD orders

---
+++ src/app-services/order/order.service.ts
@@ -0,0 +1,50 @@
+import { Inject, Service } from "@tsed/di";
+import { NotFound } from "@tsed/exceptions";
+import { OrderRequest } from "../../dtos/request/order.request";
+import { OrderResponse } from "../../dtos/response/order.response";
+import { ORDER_REPOSITORY } from "../../repositories/order/order.repository";
+import { USER_REPOSITORY } from "../../repositories/user/user.repository";
+import { OWNERSHIP_REPOSITORY } from "../../repositories/ownership/ownership.repository";
+
+@Service()
+export class OrderService {
+  @Inject(ORDER_REPOSITORY)
+  protected orderRepository: ORDER_REPOSITORY;
+
+  @Inject(USER_REPOSITORY)
+  protected userRepository: USER_REPOSITORY;
+
+  @Inject(OWNERSHIP_REPOSITORY)
+  protected ownershipRepository: OWNERSHIP_REPOSITORY;
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  public async getOrder(filter?: any): Promise<Array<OrderResponse>> {
+    const order = filter ? await this.orderRepository.find(filter) : await this.orderRepository.find();
+    if (!order) return [];
+    return order;
+  }
+
+  public async createOrder(payload: OrderRequest): Promise<OrderResponse> {
+    if (payload.id) payload.id = String(payload.id).toLowerCase();
+    return await this.orderRepository.save({ ...payload });
+  }
+
+  public async updateOrder(id: string, payload: OrderRequest): Promise<OrderResponse> {
+    const order = await this.orderRepository.findOne({ where: { id: id } });
+    if (!order) throw new NotFound("Order not found");
+
+    id = id.toLowerCase();
+    await this.orderRepository.update({ id: id }, { ...payload });
+
+    return order;
+  }
+
+  public async removeOrder(id: string): Promise<boolean> {
+    id = id.toLowerCase();
+    const order = await this.orderRepository.findOne({ where: { id: id } });
+    if (!order) throw new NotFound("Order not found");
+
+    await this.orderRepository.remove(order);
+    return true;
+  }
+}
---
+++ src/controllers/v1/order/order.controller.ts
@@ -0,0 +1,58 @@
+import { Controller, Inject } from "@tsed/di";
+import { Exception } from "@tsed/exceptions";
+import { BodyParams, PathParams, QueryParams } from "@tsed/platform-params";
+import { Get, Post, Put, Returns, Tags, Delete } from "@tsed/schema";
+import { OrderService } from "../../../app-services/order/order.service";
+import { OrderResponse } from "../../../dtos/response/order.response";
+import { OrderRequest } from "../../../dtos/request/order.request";
+import { Authenticate } from "@tsed/passport";
+
+@Controller("/order")
+@Tags("Order")
+export class OrderController {
+    @Inject(OrderService)
+    protected service: OrderService;
+
+    @Get("/")
+    @Returns(200, Array).Of(OrderResponse)
+    public async getOrder(@QueryParams("filter") filter?: string): Promise<OrderResponse[]> {
+        try {
+            return filter ? await this.service.getOrder(JSON.parse(filter)) : await this.service.getOrder();
+        } catch (error) {
+            throw new Exception(error.status, error.message);
+        }
+    }
+
+    @Post("/")
+    @Returns(200, OrderResponse)
+    public async createOrder(@BodyParams() order: OrderRequest): Promise<OrderResponse> {
+        try {
+            return await this.service.createOrder(order);
+        } catch (error) {
+            throw new Exception(error.status, error.message);
+        }
+    }
+
+    @Put("/:id")
+    @Authenticate("admin-passport")
+    @Returns(200, OrderResponse)
+    public async updateOrder(@PathParams("id") id:string, @BodyParams() order: OrderRequest): Promise<OrderResponse> {
+        try {
+            return await this.service.updateOrder(id, order);
+        } catch (err) {
+            throw new Exception(err.status, err.message);
+        }
+    }
+
+    @Delete("/:id")
+    @Authenticate("admin-passport")
+    @Returns(200, Boolean)
+    public async deleteOrder(@PathParams("id") id: string): Promise<boolean> {
+        try {
+            return await this.service.removeOrder(id);
+        } catch (err) {
+            throw new Exception(err.status, err.message);
+        }
+    }
+
+}
